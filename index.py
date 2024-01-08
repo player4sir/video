5a0fd6c973d51af6b3a97b17f4b846e20329d26c
我是海军呀 <31569037+player4sir@users.noreply.github.com>
2024-01-08 16:19:15 +0800

    Update index.py

--- index.py
+++ index.py
@@ -7,10 +7,10 @@ import json
 app = Flask(__name__)
 
 # 配置缓存
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+# cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/api')
-@cache.cached(timeout=300)
+# @cache.cached(timeout=300)
 def scrape_videos():
     page_number = request.args.get('num', default=1, type=int)
 
